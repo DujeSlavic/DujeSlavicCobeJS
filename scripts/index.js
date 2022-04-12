c806be16f02a64d3b47d8f8ce12877f21276d705
DujeSlavic <99210500+DujeSlavic@users.noreply.github.com>
2022-04-12 02:06:10 +0200

    assignment_1

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const userInput = 4
+let variable2
+
+let result = 18 + userInput
+
+result = result - 2
+result = result * 3
+result = result / 10
+
+alert('result: ' + result)
+alert('user input: ' + userInput)
